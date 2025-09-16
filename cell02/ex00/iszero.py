b8cd36deea97b3abd83d1ce1e54dca8769192e3a
tboonkan <68030075@kmitl.ac.th>
2025-09-16 18:41:36 +0000

    2ex00

--- cell02/ex00/iszero.py
+++ cell02/ex00/iszero.py
@@ -2,5 +2,5 @@ number = int(input())
 if number == 0 :
     prini ("this number is equal to Zero.")
 else :
-    print ("this number ts different from zero.")
+    print ("this number is different from zero.")
     
