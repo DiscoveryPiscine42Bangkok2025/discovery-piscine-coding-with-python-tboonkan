5879951ef1d4ae07abf18ebcad7cde1668fd289c
tboonkan <68030075@kmitl.ac.th>
2025-09-17 08:09:12 +0000

    3ex01

--- cell03/ex01/multiplication_table.py
+++ cell03/ex01/multiplication_table.py
@@ -1,11 +1,6 @@
-def main():
-    try:
-        num = int(input("Enter a number to display its multiplication table: "))
-        print(f"\nMultiplication table for {num}:")
-        for i in range(1, 13):  # You can change 13 to 11 or 21 depending on the desired range
-            print(f"{num} x {i} = {num * i}")
-    except ValueError:
-        print("Invalid input. Please enter an integer.")
-
-if __name__ == "__main__":
-    main()
+dnumber = int(input("Enter a number\n"))
+i = 0
+while i <10:
+    result = i * number
+    print(f"{i} x {number} = {result}")
+    i+=1
