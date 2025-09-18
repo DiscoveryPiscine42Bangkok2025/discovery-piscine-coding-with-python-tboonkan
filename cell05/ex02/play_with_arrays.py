4719ddf65a96084b5531882700fb37cf4798b3b5
tboonkan <68030075@kmitl.ac.th>
2025-09-18 04:42:01 +0000

    5ex02

--- cell05/ex02/play_with_arrays.py
+++ cell05/ex02/play_with_arrays.py
@@ -1,5 +1,5 @@
-password = input()
-if password == "Python is awesome":
-    print("ACCESS GRANTED")
-else :
-    print("ACCESS DENIED")
+array = [2,8,9,48,8,22,-12,2]
+new_array = [x + 2 for x in array]
+modify_array = [x for x in new_array if x > 5]
+print(array)
+print(modify_array)
