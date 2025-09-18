5e5a5f9498444829d71a2aa23e155b60d4e26566
tboonkan <68030075@kmitl.ac.th>
2025-09-18 03:35:39 +0000

    3ex03

--- cell03/ex00/to25.py
+++ cell03/ex00/to25.py
@@ -1,5 +1,5 @@
 vnumber = int(input("Enter a number less than 25\n"))
-if number < 25:
+if Number < 25:
     while number < 26:
         print(f"Inside the loop, my variable is {number}")
         number+=1
